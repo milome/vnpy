b07dcc23971cf277114bd42597638578e2df5ef7
hl l <milome@gmail.com>
2025-12-04 15:29:37 +0800

    fix: 导出 MultiTimeframeWidget

--- vnpy/chart/__init__.py
+++ vnpy/chart/__init__.py
@@ -2,6 +2,7 @@ from .widget import ChartWidget
 from .item import CandleItem, VolumeItem
 from .price_line import PriceLineItem, PriceLineManager, PriceLineType
 from .price_line_drag import PriceLineDragHandler
+from .multi_timeframe_widget import MultiTimeframeWidget
 
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     "PriceLineManager",
     "PriceLineType",
     "PriceLineDragHandler",
+    "MultiTimeframeWidget",
 ]
